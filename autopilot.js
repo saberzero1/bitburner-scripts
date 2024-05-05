1ca9779830b6db7396977fe5e674e7b0d0ae63cd
Emile Bangma <ewjbangma@hotmail.com>
2024-05-05 19:43:51 +0200

    Update autopilot.js

--- autopilot.js
+++ autopilot.js
@@ -390,7 +390,7 @@ async function checkOnRunningScripts(ns, player) {
     let goRunning = (14 in unlockedSFs) && findScript('ipvgo.js') !== undefined;
     if ((14 in unlockedSFs) && !goLaunched && !goRunning) {
         goLaunched = true;
-        const goArgs = ['--reserved-ram', 32 * 4]
+        const goArgs = ['--reserved-ram', 32 * 4, "--no-tail", false]
         if (daemonArgs.length >= 0) goArgs.push("--on-completion-script-args", JSON.stringify(daemonArgs));
         launchScriptHelper(ns, 'ipvgo.js', goArgs);
         goRunning = true;
