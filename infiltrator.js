05dddcd99a6617f84298d9cdd1fabe1b19c66d0b
Emile Bangma <ewjbangma@hotmail.com>
2024-05-28 19:22:25 +0200

    Update infiltrator.js

--- infiltrator.js
+++ infiltrator.js
@@ -190,7 +190,7 @@ export async function main(ns) {
 		}
 		else {
 			log(ns, "No Faction need Reputation, grinding money instead", verbose);
-			let maxMoney = parseShortNumber(1E15);
+			let maxMoney = options["getMoney"] == "" ? 1e13 : parseShortNumber(options["getMoney"]);
 			let stock = options["stock"];
 			await infiltrateForMoney(ns, player, maxMoney, forceTarget, stock);
 		}
