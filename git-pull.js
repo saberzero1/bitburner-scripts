fa50302eb84a2a108a66ba41ada8e4539a3c4e11
Emile Bangma <ewjbangma@hotmail.com>
2026-02-28 00:27:50 +0100

    Change default branch from 'pull-branch' to 'main'

--- git-pull.js
+++ git-pull.js
@@ -2,7 +2,7 @@ let options;
 const argsSchema = [
     ['github', 'saberzero1'],
     ['repository', 'bitburner-scripts'],
-    ['branch', 'pull-branch'],
+    ['branch', 'main'],
     ['download', []], // By default, all supported files in the repository will be downloaded. Override with just a subset of files here
     ['new-file', []], // If a repository listing fails, only files returned by ns.ls() will be downloaded. You can add additional files to seek out here.
     ['subfolder', ''], // Can be set to download to a sub-folder that is not part of the remote repository structure
